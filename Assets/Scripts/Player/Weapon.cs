a6433696cfd00194ff305166a888b9d8f17ba97a
Artorias2718 <arturoordonez2718@gmail.com>
2026-03-21 22:39:07 -0600

    Modified Weapon script so user can hold down the spacebar to shoot continuously

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -4,10 +4,12 @@ using UnityEngine.InputSystem;
 public class Weapon : MonoBehaviour
 {
     #region Properties
+    public float fireRate = 0.5f;
     public GameObject ShotPrefab;
     public Transform FirePoint;
 
     private InputAction _fireAction;
+    private float _nextFireTime = 0f;
     #endregion
 
     #region UnityEngine
@@ -17,15 +19,18 @@ public class Weapon : MonoBehaviour
         _fireAction = playerInput.actions["Fire"];
     }
 
-    private void OnEnable()
-        => _fireAction.performed += HandleFireAction;
-
-    private void OnDisable()
-        => _fireAction.performed -= HandleFireAction;
+    private void Update()
+    {
+        if(_fireAction.IsPressed() && Time.time >= _nextFireTime)
+        {
+            Fire();
+            _nextFireTime = Time.time + fireRate;
+        }
+    }
     #endregion
 
     #region Custom
-    private void HandleFireAction(InputAction.CallbackContext context)
+    private void Fire()
     {
         GameObject projectile = ProjectilePool.Instance.GetProjectile();
         if (projectile != null)
